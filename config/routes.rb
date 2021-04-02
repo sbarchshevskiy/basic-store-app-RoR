f0c205fdb494264913b7dbc05cbde225fa12f27b
vagrant <sbarchshevskiy@gmail.com>
2021-04-02 22:45:07 +0000

    implement users model

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   root to: 'products#index'
 
+  resources :sessions, only: [:create]
+  resources :registrations, only: [:create]
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
 
